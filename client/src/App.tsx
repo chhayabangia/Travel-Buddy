6dbd358300eadec614b16684b905b9cb86a419f4
Alex Marrero <alexmarrero0215@gmail.com>
2025-02-10 02:00:10 -0500

    Fixing some config files to make the red warnings go away.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
 // Main app entry point
-import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import FlightSearch from "./components/FlightSearch.tsx";
@@ -9,7 +8,6 @@ import Login from "./pages/Login";
 import "./css/global.css";
 
 function App() {
-    const [activeTab, setActiveTab] = useState("flights"); // Default tab: Flights for now
   
     return (
       <Router>
