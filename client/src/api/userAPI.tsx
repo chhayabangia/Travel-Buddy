8cff6f682e4df2ec97725273b89b7996aa905210
Peter Rudolf <peterrudolf249@gmail.com>
2025-02-10 08:41:24 -0500

    CLIENTSIDE: Made login and register pages

--- client/src/api/userAPI.tsx
+++ client/src/api/userAPI.tsx
@@ -1,13 +1,9 @@
 import Auth from "../utils/auth";
+import type { UserData } from "../interfaces/UserData";
 const DB_URL = process.env.REACT_APP_DB_URL || "http://localhost:5000";
 
-interface User {
-  id: string;
-  username: string;
-  email: string;
-}
 
-const retrieveUsers = async (): Promise<User[]> => {
+const retrieveUsers = async (): Promise<UserData[]> => {
   try {
     const response = await fetch(`${DB_URL}/api/users`, {
       headers: {
@@ -27,4 +23,89 @@ const retrieveUsers = async (): Promise<User[]> => {
   }
 };
 
-export { retrieveUsers };
+const retrieveUser = async (id: number | null): Promise<UserData> => {
+  try {
+    const response = await fetch(`${DB_URL}/api/users/${id}`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${Auth.getToken()}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("invalid user API response, check network tab!");
+    }
+
+    return await response.json();
+  } catch (err) {
+    console.error("Error from data retrieval:", err);
+    return Promise.reject("Could not fetch user info");
+  }
+}
+
+const createUser = async (body: UserData): Promise<UserData> => {
+  try {
+    const response = await fetch(`${DB_URL}/api/users`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${Auth.getToken()}`,
+      },
+      body: JSON.stringify(body),
+    });
+
+    if (!response.ok) {
+      throw new Error("invalid user API response, check network tab!");
+    }
+
+    return await response.json();
+  } catch (err) {
+    console.error("Error from user creation:", err);
+    return Promise.reject("Could not create user");
+  }
+}
+
+const updateUser = async (id: number | null, body: UserData): Promise<UserData> => {
+  try {
+    const response = await fetch(`${DB_URL}/api/users/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${Auth.getToken()}`,
+      },
+      body: JSON.stringify(body),
+    });
+
+    if (!response.ok) {
+      throw new Error("invalid user API response, check network tab!");
+    }
+
+    return await response.json();
+  } catch (err) {
+    console.error("Error from user update:", err);
+    return Promise.reject("Could not update user");
+  }
+}
+
+const deleteUser = async (id: number | null): Promise<UserData> => {
+  try {
+    const response = await fetch(`${DB_URL}/api/users/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${Auth.getToken()}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("invalid user API response, check network tab!");
+    }
+
+    return await response.json();
+  } catch (err) {
+    console.error("Error from user deletion:", err);
+    return Promise.reject("Could not delete user");
+  }
+}
+
+export { retrieveUsers, retrieveUser, createUser, updateUser, deleteUser };
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,1 +1,65 @@
 // User registration page
+import { ChangeEvent, FormEvent, useState } from "react";
+import { createUser } from "../api/userAPI";
+import type { UserData } from "../interfaces/UserData";
+import { useNavigate } from "react-router-dom";
+
+const RegisterForm = () => {
+  const [newUser, setNewUser] = useState<UserData>({
+    id: 0,
+    username: "",
+    email: "",
+    password: "",
+    createdAt: "",
+    updatedAt: "",
+  });
+
+  const navigate = useNavigate();
+
+  const createNewUser = async (body: UserData) => {
+    try {
+      const response = await createUser(body);
+      console.log(response);
+      navigate("/login");
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { name, value } = e.target;
+    setNewUser({
+      ...newUser,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    if (newUser.username && newUser.email && newUser.password) {
+      const data = createNewUser(newUser);
+      console.log(data);
+      navigate("/");
+    }
+  };
+
+  return (
+    <div className="form-container">
+    <form className="form" onSubmit={handleFormSubmit}>
+      <input
+        type="text"
+        name="username"
+        value={newUser.username}
+        onChange={handleInputChange}
+      />
+      <input
+        type="password"
+        name="password"
+        value={newUser.password}
+        onChange={handleInputChange}
+      />
+      <button type="submit">Register</button>
+    </form>
+    </div>
+  );
+};
