78f553d3db95382d8d09ad46f4f91f8f95eee245
Alex Marrero <alexmarrero0215@gmail.com>
2025-02-11 02:44:28 -0500

    Simplified server.ts to PORT 5000

--- server/src/server.ts
+++ server/src/server.ts
@@ -32,7 +32,7 @@ app.use("/api/cities", cityRoutes);
 app.use("/api/flights", flightRoutes);
 
 // ✅ Ensure Render assigns a port dynamically
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 // ✅ Start Server & Sync Database
 const startServer = async () => {
