6e0cf99c0880f87321972e2da2a2329ee0c15990
Alex Marrero <alexmarrero0215@gmail.com>
2025-02-11 02:30:45 -0500

    Simplified server.ts to match Render example

--- server/src/server.ts
+++ server/src/server.ts
@@ -22,16 +22,6 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-/*
-sequelize
-  .authenticate()
-  .then(() => console.log("Database connected successfully."))
-  .catch((err) => console.error("Database connection error:", err));
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});*/
 
 // Routes
 app.use('/api', itineraryRoutes);
@@ -42,7 +32,7 @@ app.use("/api/cities", cityRoutes);
 app.use("/api/flights", flightRoutes);
 
 // ✅ Ensure Render assigns a port dynamically
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 // ✅ Start Server & Sync Database
 const startServer = async () => {
@@ -75,6 +65,16 @@ const startServer = async () => {
   }
 };
 
+/*
+sequelize
+  .authenticate()
+  .then(() => console.log("Database connected successfully."))
+  .catch((err) => console.error("Database connection error:", err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});*/
 
 /* const startServer = async () => {
   try {
